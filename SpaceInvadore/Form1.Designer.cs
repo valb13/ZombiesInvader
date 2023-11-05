7ec5f0251348244533e030a621f5c03f24123937
Valentin BAGLAN <valentin.baglan@businfo.fr>
2023-11-05 15:06:12 +0100

    Déplacements joueur sur la carte

--- SpaceInvadore/Form1.Designer.cs
+++ SpaceInvadore/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SpaceInvadore
 {
-    partial class Form1
+    partial class ZombieGame
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,18 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            player = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
             SuspendLayout();
             // 
-            // Form1
+            // flowLayoutPanel1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(0, 0);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // player
+            // 
+            player.Image = ZombiesInvader.Properties.Resources.up;
+            player.Location = new Point(509, 472);
+            player.Name = "player";
+            player.Size = new Size(71, 100);
+            player.SizeMode = PictureBoxSizeMode.AutoSize;
+            player.TabIndex = 2;
+            player.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimer_Tick;
+            // 
+            // ZombieGame
+            // 
+            AutoScaleDimensions = new SizeF(11F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(778, 844);
-            Name = "Form1";
-            Text = "Form1";
+            BackColor = Color.SlateGray;
+            ClientSize = new Size(1076, 655);
+            Controls.Add(player);
+            Controls.Add(flowLayoutPanel1);
+            Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "ZombieGame";
+            Text = "Zombie Game";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
--- SpaceInvadore/Form1.cs
+++ SpaceInvadore/Form1.cs
@@ -1,10 +1,92 @@
 namespace SpaceInvadore
 {
-    public partial class Form1 : Form
+    public partial class ZombieGame : Form
     {
-        public Form1()
+        #region variables
+        string direction = "up"; // direction du joueur
+        bool goLeft, goRight, goUp, goDown; // booléens pour savoir si le joueur va dans une direction
+        int playerSpeed = 10; // vitesse du joueur
+        int playerLeft; // position du joueur en abscisse
+        int playerTop; // position du joueur en ordonnée
+        #endregion
+
+
+        public ZombieGame()
         {
             InitializeComponent();
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e) //action à réaliser quand une touches est relachée
+        {
+            switch (e.KeyCode) // on regarde quelle touche est relachée et on adapte la direction et l'image du joueur en fonction
+            {
+                case Keys.Z:
+                    goUp = false;
+                    break;
+                case Keys.S:
+                    goDown = false;
+                    break;
+                case Keys.Q:
+                    goLeft = false;
+                    break;
+                case Keys.D:
+                    goRight = false;
+                    break;
+            }
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e) //action à réaliser quand une touches est préssée
+        {
+            switch (e.KeyCode) // on regarde quelle touche est préssée et on adapte la direction et l'image du joueur en fonction
+            {
+                case Keys.Z:
+                    direction = "up";
+                    goUp = true;
+                    player.Image = ZombiesInvader.Properties.Resources.up;
+                    break;
+                case Keys.S:
+                    direction = "down";
+                    goDown = true;
+                    player.Image = ZombiesInvader.Properties.Resources.down;
+                    break;
+                case Keys.Q:
+                    direction = "left";
+                    goLeft = true;
+                    player.Image = ZombiesInvader.Properties.Resources.left;
+                    break;
+                case Keys.D:
+                    direction = "right";
+                    goRight = true;
+                    player.Image = ZombiesInvader.Properties.Resources.right;
+                    break;
+                case Keys.Space:
+                    break;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            playerLeft = player.Left; // on récupère la position du joueur en abscisse
+            playerTop = player.Top; // on récupère la position du joueur en ordonnée
+
+            if (goLeft == true && playerLeft > 0) // si le joueur va à gauche et qu'il n'est pas au bord de l'écran
+            {
+                player.Left -= playerSpeed; // on déplace le joueur vers la gauche
+            }
+            if (goRight == true && playerLeft + player.Width < this.ClientSize.Width) // si le joueur va à droite et qu'il n'est pas au bord de l'écran
+            {
+                player.Left += playerSpeed; // on déplace le joueur vers la droite
+            }
+            if (goUp == true && playerTop > 45) // si le joueur va en haut et qu'il n'est pas au bord de l'écran
+            {
+                player.Top -= playerSpeed; // on déplace le joueur vers le haut
+            }
+            if (goDown == true && playerTop + player.Height < this.ClientSize.Height) // si le joueur va en bas et qu'il n'est pas au bord de l'écran
+            {
+                player.Top += playerSpeed; // on déplace le joueur vers le bas
+            }
+
         }
     }
 }
